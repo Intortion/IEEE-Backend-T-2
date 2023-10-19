db1c46fa1961dcfdc8f23582afd5fa112df80aa2
Intortion <144892095+Intortion@users.noreply.github.com>
2023-10-20 01:07:56 +0530

    Updated checkout
    checkout page is now in sync with the backend

--- django_project/customer/urls.py
+++ django_project/customer/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     path('', views.store, name='customer-store'),
     path('cart/', views.cart, name='customer-cart'),
-    path('checkout/', views.checkout, name='customer-checkout')
+    path('checkout/', views.checkout, name='customer-checkout'),
+    path('update_item/', views.updateItem, name='customer-update_item'),
+    path('process_order/', views.processOrder, name='customer-process_order')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
