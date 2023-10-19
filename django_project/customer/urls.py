54173e69b16c687fb14a0403e6a942cea3b8082a
Intortion <144892095+Intortion@users.noreply.github.com>
2023-10-19 14:42:34 +0530

    Admin page
    Added admin page, home page, created databases for products, customers, orders..

--- django_project/customer/urls.py
+++ django_project/customer/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.store, name='customer-store'),
     path('cart/', views.cart, name='customer-cart'),
     path('checkout/', views.checkout, name='customer-checkout')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -120,9 +120,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'customer','static/')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'customer','static','images')
+
+MEDIA_URL = '/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
